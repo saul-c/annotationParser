ae768d26390772872bd5ca25de661c7b520da865
陈硕 <chenshuo@touker.com>
2020-10-29 16:02:38 +0800

    update test

--- src/com/annotation/test/Child.java
+++ src/com/annotation/test/Child.java
@@ -1,10 +1,10 @@
 package com.annotation.test;
 
-@Description(desc="这是类注解",author="ashuo",age=23)
+@Description(desc="这是类注解",author="alex",age=23)
 public class Child implements Person{
 
 	@Override
-	@Description(desc="这是方法注解",author="ashuo",age=23)
+	@Description(desc="这是方法注解",author="alex",age=23)
 	public String name() {
 		// TODO Auto-generated method stub
 		return null;
